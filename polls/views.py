469c689eb68beb725e46cc1ce49277c8fb5008f7
Travis Hunt <thunt@oxygen.cs.dixie.edu>
2013-08-25 13:51:04 -0600

    poll urls are working

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Why, hello there!  You found the index for Polls!")
+
+def detail(request, poll_id):
+	return HttpResponse("This is poll %s." % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("Yup, you found the poll results page of poll %s." % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("This is were you vote on the poll %s." % poll_id)
+
