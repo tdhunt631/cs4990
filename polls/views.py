33f24395defa493753e7d3c0daaaabb1d7e03da1
Travis Hunt <tdhunt631@gmail.com>
2013-08-25 14:49:50 -0600

    finished with part 3

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 def index(request):
-	return HttpResponse("Why, hello there!  You found the index for Polls!")
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("This is poll %s." % poll_id)
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
 	return HttpResponse("Yup, you found the poll results page of poll %s." % poll_id)
