2dce362492cb1506b38cc284277b72e8eb62dd96
Travis Hunt <tdhunt631@gmail.com>
2013-08-27 21:02:48 -0600

    finished writing view tests

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from polls.models import Poll
 from django.views import generic
+from django.utils import timezone
+
 
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
@@ -11,15 +13,23 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		"""Return the last five published polls."""
-		return Poll.objects.order_by('-pub_date')[:5]
+		return Poll.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/detail.html'
 
+	def get_queryset(self):
+		return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
 class ResultsView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/results.html'
+
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
